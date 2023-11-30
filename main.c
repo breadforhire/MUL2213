1045e19128e775b6fc5a385b4b58cc0323c48955
Michael Metyk <126725193+breadforhire@users.noreply.github.com>
2023-11-30 16:55:23 -0500

    Update main.c

--- main.c
+++ main.c
@@ -9,11 +9,6 @@
 
 #define c_t 2
 
-__attribute__((__aligned__(32)))
-static const int32_t x_n[4] = {0, 1, 2, 3};
-
-__attribute__((__aligned__(32)))
-static int32_t y_n [4] = {0, 1, 2, 3};
 
 #define PRINT(vector) \
     do { \
@@ -22,12 +17,26 @@ static int32_t y_n [4] = {0, 1, 2, 3};
         } \
     } while (0)
 
+__attribute__((__aligned__(32)))
+static const int32_t x_n[4] = {0, 1, 2, 3};
+
+__attribute__((__aligned__(32)))
+static int32_t y_n [4] = {0, 1, 2, 3};
+
+
+__attribute__((__aligned__(32)))
+static int32_t c_24 [4] = {24, 24, 24, 24};
+
+
+
+
 void A()
 {
 
  uint32_t temp[4];
  __m128i x = _mm_load_si128((__m128i*)x_n);
  __m128i y = _mm_load_si128((__m128i*)  y_n);
+ __m128i c_24_a = _mm_load_si128((__m128i*)c_24);
 
   /*a registers*/
   __m128i a0 = _mm_set1_epi32(x_n[0] * y_n[0]);
@@ -40,6 +49,12 @@ void A()
   __m128i b1 = _mm_add_epi32(b2, a1);
   __m128i b0 = _mm_add_epi32(b1, a0);
 
+  /*setting up z registers*/
+  __m128i z0;
+  __m128i z1;
+  __m128i z2;
+  __m128i z3;
+
  /* t0 ← b0 − (x0 − x3)(y0 − y3) − (x1 − x2)(y1 − y2) */
   __m128i t0 = _mm_mullo_epi32 (_mm_sub_epi32(_mm_set1_epi32(x_n[0]), _mm_set1_epi32(x_n[3])), _mm_sub_epi32(_mm_set1_epi32(y_n[0]), _mm_set1_epi32(y_n[3])));
   t0 = _mm_sub_epi32(b0, t0);
@@ -47,7 +62,50 @@ void A()
   _mm_store_si128((__m128i*)temp, t0);
 
   /*z3 ← t0 mod t*/
-  __m128i z3 = _mm_set1_epi32(temp[0] % c_t);
+  z3 = _mm_set1_epi32(temp[0] % c_t);
+  __m128i t1 = _mm_mullo_epi32 (_mm_sub_epi32(_mm_set1_epi32(x_n[1]), _mm_set1_epi32(x_n[3])), _mm_sub_epi32(_mm_set1_epi32(y_n[1]), _mm_set1_epi32(y_n[3])));
+  t1 = _mm_sub_epi32(b1, t1);
+  t1 = _mm_add_epi32(a0, _mm_mullo_epi32(t1, c_24_a));
+  t1 =  _mm_add_epi32(_mm_slli_epi64(t0, 56), t1);
+
+ _mm_storeu_si128((__m128i*)temp, t1);
+ z0 = _mm_set1_epi32 (temp[0] % c_t);
+ a0 = _mm_add_epi32(a0, a1);
+ /* t0 ← a0 − (x0 − x1)(y0 − y1) + 24(b2 − (x2 − x3)(y2 − y3)) + (t1 >> 56) */
+ t0 = _mm_add_epi32(_mm_slli_epi64(t1, 56), t0);
+ t0 = _mm_sub_epi32(b2, _mm_mullo_epi32 (_mm_sub_epi32(_mm_set1_epi32(x_n[2]), _mm_set1_epi32(x_n[3])), _mm_sub_epi32(_mm_set1_epi32(y_n[2]), _mm_set1_epi32(y_n[3]))));
+ t0 = _mm_mullo_epi32(t0, c_24_a);
+ t0  = _mm_add_epi32(_mm_sub_epi32(a0, _mm_mullo_epi32 (_mm_sub_epi32(_mm_set1_epi32(x_n[0]), _mm_set1_epi32(x_n[1])), _mm_sub_epi32(_mm_set1_epi32(y_n[0]), _mm_set1_epi32(y_n[1])))), t0);
+
+
+ _mm_storeu_si128((__m128i*)temp, t0);
+ z1 = _mm_set1_epi32(temp[0] % c_t);
+
+ /* t1 ← a0 + a2 + 24a3 − (x0 − x2)(y0 − y2) + (t0 >> 56) */
+ t1 = _mm_add_epi32(a0, a2);
+ t1 = _mm_add_epi32(_mm_mullo_epi32(c_24_a, a3), t1);
+ t1 = _mm_sub_epi32( t1,  _mm_mullo_epi32 (_mm_sub_epi32(_mm_set1_epi32(x_n[0]), _mm_set1_epi32(x_n[1])), _mm_sub_epi32(_mm_set1_epi32(y_n[0]), _mm_set1_epi32(y_n[1]))));
+
+
+ _mm_storeu_si128((__m128i*)temp, t1);
+ z2 = _mm_set1_epi32(temp[0] % c_t);
+
+ /* we can store this in a variable, so we do not have to do the operations twice but results are negilibile as of last benchmarking */
+ t0 = _mm_add_epi32(z3, _mm_slli_epi64(t1, 58));
+
+ _mm_storeu_si128((__m128i*)temp, t0);
+ z3 = _mm_set1_epi32(temp[0] % c_t);
+ z0 = _mm_add_epi32(z0, _mm_mullo_epi32(c_24_a, _mm_slli_epi64(t1, 58)));
+
+
+
+
+
+
+
+
+
+
 
 
 
